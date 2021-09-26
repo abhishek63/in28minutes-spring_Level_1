c4e0e6ee1e72d88e3d4fdb6a7432c4cdeb81d86a
Abhishek Kumar <kumargolu63@gmail.com>
2021-09-26 13:36:20 +0530

    removing springboot from application

--- src/main/java/com/abhishek63/github/spring28minuteslevel1/Spring28minuteslevel1Application.java
+++ src/main/java/com/abhishek63/github/spring28minuteslevel1/Spring28minuteslevel1Application.java
@@ -1,11 +1,14 @@
 package com.abhishek63.github.spring28minuteslevel1;
 
 import com.abhishek63.github.spring28minuteslevel1.basic.BinarySearchImpl;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
-@SpringBootApplication
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
 public class Spring28minuteslevel1Application {
 
 	public static void main(String[] args) {
@@ -16,7 +19,7 @@ public class Spring28minuteslevel1Application {
 //		BinarySearchImpl binarySearch = new BinarySearchImpl(new QuickSortAlgorithm());
 //		binarySearch.searchElement(new int[] {1,2,4},2);
 
-		 ApplicationContext applicationContext = SpringApplication.run(Spring28minuteslevel1Application.class, args);
+		 ApplicationContext applicationContext = new AnnotationConfigApplicationContext(Spring28minuteslevel1Application.class);
 		 BinarySearchImpl binarySearch1 = applicationContext.getBean(BinarySearchImpl.class);
 		System.out.println(binarySearch1);
 		binarySearch1.setX(67);
