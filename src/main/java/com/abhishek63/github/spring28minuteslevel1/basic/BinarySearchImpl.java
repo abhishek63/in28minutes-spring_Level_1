2f8c7a9bf25619e580df69c65aaa0bd4c7ebe810
Abhishek Kumar <kumargolu63@gmail.com>
2021-09-26 13:06:14 +0530

    add @PostConstruct and @PreDestroy

--- src/main/java/com/abhishek63/github/spring28minuteslevel1/basic/BinarySearchImpl.java
+++ src/main/java/com/abhishek63/github/spring28minuteslevel1/basic/BinarySearchImpl.java
@@ -1,13 +1,20 @@
 package com.abhishek63.github.spring28minuteslevel1.basic;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
 @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class BinarySearchImpl {
+
+    Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     private SortingAlgo sortingAlgo ;
@@ -32,5 +39,15 @@ public class BinarySearchImpl {
         return 3;
     }
 
+    @PostConstruct
+    public void postConstruct(){
+       logger.info("postConstruct");
+    }
+
+    @PreDestroy
+    public void preDestroy(){
+        System.out.println("predestroy in fun");
+        logger.info("preDestroy");
+    }
 
 }
